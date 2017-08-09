4ae8ac81aacd10155712347f4184c3d992dc821b
Jason Lattimer <jason.lattimer@gmail.com>
2017-08-08 20:48:49 -0500

    clean up

--- generator-d365/__tests__/app.js
+++ generator-d365/__tests__/app.js
@@ -2,23 +2,11 @@
 var path = require('path');
 var assert = require('yeoman-assert');
 var helpers = require('yeoman-test');
-// Var os = require('os');
 
 describe('generator-d365:app', () => {
   beforeAll(() => {
-    // Var options = {
-    //   testPath: path.join(os.tmpdir(), 'crmtest')
-    // };
-
-    // var tmpDir = path.join(os.tmpdir(), 'crmtest');
-    // helpers.testDirectory(tmpDir, function (err) {
-    //   if (err) {
-    //     // Done(err);
-    //   }
-    // });
 
     return helpers.run(path.join(__dirname, '../generators/app'))
-      // .withOptions(options)
       .withPrompts({
         projectType: 'Console 2016+ Project',
         sdkVersion: '8.2.0.2',
@@ -29,8 +17,6 @@ describe('generator-d365:app', () => {
   it('creates files', () => {
     assert.file([
       'Program.cs'
-      // ,
-      // 'crmtest.csproj'
     ]);
   });
 });
